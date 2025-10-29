ab76f1504e6c3337cc364eb410518e4c42055b7a
WhoisJulie007 <maydeli.castan@iest.edu.mx>
2025-10-29 15:15:59 -0600

    Initial Commit

---
+++ AppRentameYA/AppRentameYAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppRentameYAApp.swift
+//  AppRentameYA
+//
+//  Created by Maydeli Castan on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AppRentameYAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppRentameYATests/AppRentameYATests.swift
@@ -0,0 +1,17 @@
+//
+//  AppRentameYATests.swift
+//  AppRentameYATests
+//
+//  Created by Maydeli Castan on 29/10/25.
+//
+
+import Testing
+@testable import AppRentameYA
+
+struct AppRentameYATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
