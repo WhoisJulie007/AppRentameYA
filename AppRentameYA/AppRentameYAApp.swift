86655892826edc821e273ee4fcdf0715f6b1dbf0
Julia <maydeli.castan@iest.edu.mx>
2025-11-12 18:53:57 -0600

    paso 1 de agregar la base de datos fire base

--- AppRentameYA/AppRentameYAApp.swift
+++ AppRentameYA/AppRentameYAApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
 
 @main
 struct AppRentameYAApp: App {
+    
+    // Se ejecuta al iniciar la app
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
